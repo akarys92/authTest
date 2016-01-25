5c5921a01e9399bd8a7d76ad8d4ba08926f31179
Alexander Karys <akarys92@Alexander-Karyss-MacBook-Pro-2.local>
2016-01-24 21:54:30 -0800

    Added git funcs

--- authTest/urls.py
+++ authTest/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include('auth.urls')),
+    url(r'^authTest/', include('authApp.urls')),
 )
