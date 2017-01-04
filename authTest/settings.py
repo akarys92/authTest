2f67d3111b4a259086990e5ee50cfab9dfbaf0a9
akarys92 <akarys92@gmail.com>
2017-01-04 12:19:46 -0800

    Update settings.py

--- authTest/settings.py
+++ authTest/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x@k_f(6z+%tjes2e(%!uir&646l#r07l+!!nv!cy0kn#*2z@o='
+#SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
